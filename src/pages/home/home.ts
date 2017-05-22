7c533864314650750ffbddf65fcd22e1d6503363
claidson <cchaidrich@hotmail.com>
2017-05-22 08:50:16 -0300

    primeiro commit 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+public event = {
+    month: '1990-02-19',
+    timeStarts: '07:43',
+    timeEnds: '1990-02-20'}
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
